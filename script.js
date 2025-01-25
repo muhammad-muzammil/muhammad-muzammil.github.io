f62dfd06c6533c542340262edf7fca7a094305c9
muhammad-muzammil <mmuzammil@cs.stonybrook.edu>
2025-01-25 01:23:38 +0000

    js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-document.querySelector('.dropdown-btn').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.dropdown-btn');
     const dropdownContent = document.querySelector('.dropdown-content');
-    if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
-      dropdownContent.style.display = 'block'; // Show the citation
-      this.textContent = 'Hide Citation'; // Update button text
-    } else {
-      dropdownContent.style.display = 'none'; // Hide the citation
-      this.textContent = 'Show Citation'; // Update button text
-    }
+  
+    button.addEventListener('click', function () {
+      if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
+        dropdownContent.style.display = 'block'; // Show citation
+        this.textContent = 'Hide Citation'; // Update button text
+      } else {
+        dropdownContent.style.display = 'none'; // Hide citation
+        this.textContent = 'Show Citation'; // Update button text
+      }
+    });
   });
